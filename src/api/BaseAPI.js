948c1e9e5f6fc7c0fb882967bce1536a2b7a3e72
Davison Vinicius <davison.vinicius360@gmail.com>
2023-12-04 15:46:51 -0300

    ajusta api

--- src/api/BaseAPI.js
+++ src/api/BaseAPI.js
@@ -2,6 +2,11 @@ import axios from 'axios'
 
 const BaseAPI = axios.create({
     baseURL: 'https://api.globomktcorretora.com/api/v1/',
+    withCredentials: true,
+    headers: {
+    'Content-Type': 'application/json',
+    'X-Requested-With': 'XMLHttpRequest',
+  },
     
 })
 
